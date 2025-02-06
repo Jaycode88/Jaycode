132c4665626e0e7f1102aa8bc9c25343efd20655
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2025-02-06 11:03:47 +0000

    Add Terms&conditions as own page, Remove policy modals

--- app.py
+++ app.py
@@ -121,6 +121,10 @@ def index():
 def privacy():
     return render_template("privacy.html")
 
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
